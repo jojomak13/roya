547cd316abdf309cb116cea950d20a76b5aacde9
jojomak13 <jojomak350@gmail.com>
2020-01-14 22:01:28 +0200

    add dropdown to change language

--- resources/views/layouts/user/_header.blade.php
+++ resources/views/layouts/user/_header.blade.php
@@ -1,14 +1,28 @@
 <header>
     <div class="container">
-        <div class="top-nav d-flex justify-content-end">
+        <div class="top-nav d-flex justify-content-between">
             <ul>
-            @if(auth()->user())
+                @if(auth()->user())
                 <li><a href="{{ route('profile') }}" title="@lang('user.header.myAccount')"><i class="fa fa-user"></i> @lang('user.header.myAccount')</a></li>
-            @else
+                @else
                 <li><a href="{{ route('login') }}" title="@lang('user.header.login')"><i class="fa fa-lock"></i> @lang('user.header.login')</a></li>
-            @endif
+                @endif
                 <li><a href="#" title="@lang('user.header.wishlist')"><i class="fa fa-heart"></i> @lang('user.header.wishlist')</a></li>
                 <li><a href="{{ route('cart.index') }}" title="@lang('user.header.mycart')"><i class="fa fa-shopping-cart"></i> @lang('user.header.mycart')</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <div class="language dropdown">
+                        <button class="dropdown-toggle" type="button" id="language" data-toggle="dropdown" aria-haspopup="true">
+                            <i class="fa fa-globe"></i> {{ LaravelLocalization::getCurrentLocaleName() }}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="language">
+                            @foreach(LaravelLocalization::getSupportedLocales() as $key => $lang)
+                                <a href="{{ LaravelLocalization::getLocalizedURL($key, null, [], true) }}" class="dropdown-item">{{ $lang['name'] }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </li>
             </ul>
         </div>
         <div class="middle-nav d-flex justify-content-between">
--- resources/views/layouts/user/_navbar.blade.php
+++ resources/views/layouts/user/_navbar.blade.php
@@ -13,22 +13,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('blog.index') }}">@lang('user.title.blog')</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Shoes</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Pages
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="./category.html">Category</a>
-                            <a class="dropdown-item" href="./product.html">Product</a>
-                            <a class="dropdown-item" href="./cart.html">Cart</a>
-                            <a class="dropdown-item" href="./checkout.html">Checkout</a>
-                            <a class="dropdown-item" href="#">Profile</a>
-                        </div>
-                    </li>
                 </ul>
             </div>
         </div>
