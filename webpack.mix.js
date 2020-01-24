3f691cc9b0b30da8403202a5f4a2156a926a4858
jojomak13 <jojomak350@gmail.com>
2020-01-24 03:48:48 +0200

    optimize user css files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/admin/app.js', 'public/admin')
-   .sass('resources/sass/app.scss', 'public/admin/');
+mix.js("resources/js/admin/app.js", "public/admin").sass(
+    "resources/sass/app.scss",
+    "public/admin/"
+);
+
+mix.sass("resources/sass/user/app.scss", "public/user/css/").version();
